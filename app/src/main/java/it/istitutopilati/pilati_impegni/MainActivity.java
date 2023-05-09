49f38d21ab853e86625fab07da650bf8e5385897
maxglio <massimo.maglio.06@gmail.com>
2023-05-09 17:30:39 +0200

    prova

--- app/src/main/java/it/istitutopilati/pilati_impegni/MainActivity.java
+++ app/src/main/java/it/istitutopilati/pilati_impegni/MainActivity.java
@@ -3,15 +3,55 @@ package it.istitutopilati.pilati_impegni;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView ListViewImpegni;
+    private EditText editImpegni;
+
+    private ArrayList <String> listaImpegni;
+    private ArrayAdapter <String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListViewImpegni = (ListView) findViewById(R.id.ListViewImpegni);
+        editImpegni = (EditText) findViewById(R.id.editTextImpegni);
+
+        listaImpegni = new ArrayList<String>();
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        ListViewImpegni.setAdapter(adapter);
+
+        ListViewImpegni.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+
+            }
+        });
+
+
     }
+    ArrayList<String> elenco = new  ArrayList<String>();
 
+    public void aggiungiImpegno(View v){
+        String nuovoImpegno = editImpegni.getText().toString();
+        if(nuovoImpegno.length() > 0){
+            listaImpegni.add(0, nuovoImpegno);
+        }
+        Log.d("MainActivity", editImpegni.getText().toString());
+        adapter.notifyDataSetChanged();
 
-    //prova
+        editImpegni.setText("");
+    }
 }
